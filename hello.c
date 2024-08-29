c247ea11ea15ea23bb8b52e2a536685e01883f0d
PeytonToussaint <ptoussaint2@unl.edugit config --global user.name Peyton>
2024-08-29 12:39:01 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Peyton Toussaint
+ *         ptoussaint@unl.edu
+ * Date: 8/27/2024
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Peyton Toussaint\n");
+
+  return 0;
+}
